0135e5ebe49c75b6b502a17dc26659c9c99221e5
RolandFornvald <r.fornvald@students.htl-leonding.ac.at>
2025-04-28 09:25:34 +0200

    Created method for checking neighbors

--- micro-mouse/backend/generator.ts
+++ micro-mouse/backend/generator.ts
@@ -9,3 +9,8 @@ function huntAndKill(width: number, height: number) : number[][]{
     //TO_DO: Implement Hunt & Kill algorithm
     return maze;
 }
+function neighbors(maze: number[][], a: number, b: number): number[][] {
+    let final: number[][] = [];
+    //TO_DO: Implement checker for the neighbors
+    return final;
+}
