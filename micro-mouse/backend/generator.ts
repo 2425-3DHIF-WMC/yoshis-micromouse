2f7bb27da76ab4d95435d74bc4b35c2d00550ccf
RolandFornvald <r.fornvald@students.htl-leonding.ac.at>
2025-04-28 09:19:00 +0200

    Created method where maze generation is mainly used

---
+++ micro-mouse/backend/generator.ts
@@ -0,0 +1,11 @@
+function huntAndKill(width: number, height: number) : number[][]{
+
+    width -= width % 2;
+    width++;
+    height -= height % 2;
+    height++;
+
+    let maze: number[][] = [];
+    //TO_DO: Implement Hunt & Kill algorithm
+    return maze;
+}
