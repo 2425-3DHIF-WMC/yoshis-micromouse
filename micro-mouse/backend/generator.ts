9a2af93f8bea89b57bdc5f89b12711278c1df688
RolandFornvald <r.fornvald@students.htl-leonding.ac.at>
2025-04-28 18:23:40 +0200

    Implemented neighbor searcher (to-replace and to see if next to select is valid)

--- micro-mouse/backend/generator.ts
+++ micro-mouse/backend/generator.ts
@@ -12,12 +12,35 @@ function huntAndKill(width: number, height: number) : number[][]{
 function neighbors(maze: number[][], a: number, b: number): number[][] {
     let final: number[][] = [];
     //TO_DO: Implement checker for the neighbors
+    for(let i: number = 0; i < 4; i++){
+        let n: number[] = [a,b];
+        n[i%2] += ((Math.floor(i / 2) * 2) || -2);
+
+        if( n[0] < maze.length &&
+            n[1] < maze.length &&
+            n[0] > 0 && n[1] > 0){
+
+            if(maze[n[0]][n[1]] == 1){      // Replaces wall with empty space
+                final.push(n);
+            }
+        }
+    }
     return final;
 }
 
 function neighborsAB(maze: number[][], a: number, b: number): number[][]{
-    let final: number[][] = [];
     //TO_DO: Implement method for coordinate checking (NOT the same as the method "neighbors"
+    let final: number[][] = [];
+    for(let i: number = 0; i < 4; i++){             // Iterates through the 4 closest neighbors
+        let n: number[] = [a, b];                   // (checks if they are reachable)
+        n[i % 2] += ((Math.floor(i / 2) * 2) || -2);
+
+        if(n[0] < maze.length &&            //Checks edge cases (corner, edge)
+           n[1] < maze[0].length &&
+            n[0] > 0 && n[1] > 0){
+            final.push(n);
+        }
+    }
     return final;
 }
 
@@ -37,7 +60,6 @@ function findCoordinate(maze: number[][]){
     //TO_DO: Implement method for finding the coordinates for neighbors (basically where to go)
     for(let i: number = 1; i < maze.length; i+=2){
         for(let j: number = 1; j < maze.length; j+=2){
-
             if(maze[i][j] == 1){
                 const neighbor: number[][] = neighborsAB(maze, i, j);
 
