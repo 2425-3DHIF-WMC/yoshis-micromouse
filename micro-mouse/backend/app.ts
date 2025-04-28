cf16a0fe948ce638a5a3f430119331f8466710e4
RolandFornvald <r.fornvald@students.htl-leonding.ac.at>
2025-04-28 09:14:18 +0200

    Created generator.ts and made a route for it

--- micro-mouse/backend/app.ts
+++ micro-mouse/backend/app.ts
@@ -7,6 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api",);
+app.use("/api/generateMaze");
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
---
+++ micro-mouse/backend/generator.ts
@@ -0,0 +1,12 @@
+function huntAndKillMaze(width: number, height: number) {
+
+    width -= width % 2;
+    width++;
+    height -= height % 2;
+    height++;
+
+
+    let maze: number[][] = [];
+
+    return maze;
+}
